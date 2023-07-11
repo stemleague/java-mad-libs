bccf05c6ecfe7dfc6d015469edef07a2d0693966
stem league <64894084+stemleague@users.noreply.github.com>
2023-07-11 08:11:25 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,33 @@
+public class Main {
+  // A code statement with / in the front means it is a Java comment.
+  //We are going to be making our own Mad Libs game! 
+  public static void main(String[] args) {
+    // Part 1: Defining Input Variables 
+    //Let's define some variables you may want to use below, in Mad Libs we want to work with INPUTS in this game,
+    //so we want to declare a variable to an input. We can do this by writing:
+    String myNumber = "5";
+    System.out.println("This is the number you typed in " + myNumber);
+    System.out.println("Hello world!");
+
+
+    // Part 2: Walk Through an Example Mad Lib 
+    // Alright now, we have some variables, let's write print statements that we want
+    // to print out from our Mad Libs. Keep in mind we will be concatenating (adding) our
+    // variables together.
+    // Try making some variables you want to use in your Mad Libs program and make sure to add an 
+    // input() function that takes in the prompt to ask the user.
+    // Here's an example below:
+    String adjective = ""; // TODO: Add adjective here
+    String adjective2 = ""; // TODO: Add adjective here
+    String room_in_a_house = ""; // // TODO: Add room here
+    System.out.println("It was a " + adjective + ", cold November day. I woke up to the " + adjective2 + " smell of roasting in the " + room_in_a_house + " downstairs.");
+    System.out.println(); // empty line
+    
+    
+    // Part 3: Making your own Mad Libs
+    // Now it is time for your to go on & make your own Mad Libs! Be creative & fun :) 
+    // Feel free to use the example from above for some inspiration!
+    System.out.println("Let's start writing your own Mad Libs below! :)");
+    
+  }
+}
